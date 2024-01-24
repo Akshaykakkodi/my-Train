6998ae649ad15c2dfad097736321d110c8817740
Akshay kumar <akshaykumarp333@gmail.com>
2024-01-24 10:37:23 +0530

    api key changed

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class AppConstants{
- static String apiKey="eb54cc4784msh324dc617fbcf1b5p1e8404jsnd29f8879b8b6";
+ static String apiKey="b593a56b7cmsheb831f889e0f0fbp1e0358jsn65aa48709285";
  static String baseUrl="https://irctc1.p.rapidapi.com/api/";
  static Color primaryColor=const Color(0xff7D3C98);
 }
